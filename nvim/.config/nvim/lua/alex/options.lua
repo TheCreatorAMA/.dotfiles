ad0516b2207c9df6af18e9f11d2e3bacce80992f
TheCreatorAMA <alexanderakiu@gmail.com>
2022-06-06 10:14:56 -0700

    Use for loop to load options

--- nvim/.config/nvim/lua/alex/options.lua
+++ nvim/.config/nvim/lua/alex/options.lua
@@ -3,47 +3,54 @@
 -- setting vim.opt to a variable so 
 -- no need to keep typing it out
 
-vo = vim.opt
+local options = {
+  backup = false,                         -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                        -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                            -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- insert -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                       -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = false,                      -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = true,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                            -- display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+}
 
-vo.backup = false                          -- creates a backup file
-vo.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vo.cmdheight = 2                           -- more space in the neovim command line for displaying messages
-vo.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-vo.conceallevel = 0                        -- so that `` is visible in markdown files
-vo.fileencoding = "utf-8"                  -- the encoding written to a file
-vo.hlsearch = true                         -- highlight all matches on previous search pattern
-vo.ignorecase = true                       -- ignore case in search patterns
-vo.mouse = "a"                             -- allow the mouse to be used in neovim
-vo.pumheight = 10                          -- pop up menu height
-vo.showmode = false                        -- we don't need to see things like -- insert -- anymore
-vo.showtabline = 2                         -- always show tabs
-vo.smartcase = true                        -- smart case
-vo.smartindent = true                      -- make indenting smarter again
-vo.splitbelow = true                       -- force all horizontal splits to go below current window
-vo.splitright = true                       -- force all vertical splits to go to the right of current window
-vo.swapfile = false                        -- creates a swapfile
--- vo.termguicolors = true                    -- set term gui colors (most terminals support this)
-vo.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vo.undofile = true                         -- enable persistent undo
-vo.updatetime = 300                        -- faster completion (4000ms default)
-vo.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vo.expandtab = true                        -- convert tabs to spaces
-vo.shiftwidth = 2                          -- the number of spaces inserted for each indentation
-vo.tabstop = 2                             -- insert 2 spaces for a tab
-vo.cursorline = false                      -- highlight the current line
-vo.number = true                           -- set numbered lines
-vo.relativenumber = true                  -- set relative numbered lines
-vo.numberwidth = 4                         -- set number column width to 2 {default 4}
-vo.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vo.wrap = false                            -- display lines as one long line
-vo.scrolloff = 8                           -- is one of my fav
-vo.sidescrolloff = 8
-vo.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+
 
 -- to set a new option just do 
--- vo.<option found in :help options menu>
+-- <option found in :help options menu>
 
+vo = vim.opt
 vo.shortmess:append "c"
+
+for key, value in pairs(options) do
+  vo[key] = value
+end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
