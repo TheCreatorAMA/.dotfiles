2c754cb20a744bf9f46e06186cb464e757ef8c6c
TheCreatorAMA <alexanderakiu@gmail.com>
2022-06-13 07:54:10 -0700

    Updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,16 @@
 -- This is where nvim looks for all "imports"
 -- Require any new lua files here
 
-require "alex.options"
-require "alex.keymaps"
-require "alex.plugins"
-require "alex.colorscheme"
-require "alex.cmp"
-require "alex.lsp"
-require "alex.telescope"
-require "alex.treesitter"
-require "alex.autopairs"
-require "alex.comment"
-require "alex.gitsigns"
-require "alex.nvim-tree"
-require "alex.bufferline"
+require("alex.options")
+require("alex.keymaps")
+require("alex.plugins")
+require("alex.colorscheme")
+require("alex.cmp")
+require("alex.lsp")
+require("alex.telescope")
+require("alex.treesitter")
+require("alex.autopairs")
+require("alex.comment")
+require("alex.gitsigns")
+require("alex.nvim-tree")
+require("alex.bufferline")
