eb7ba2fa6a6ac01ab5d2d77ea1b4b8651649fc2e
TheCreatorAMA <alexanderakiu@gmail.com>
2022-06-13 08:11:54 -0700

    Comments

--- nvim/.config/nvim/lua/alex/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/alex/lsp/null-ls.lua
@@ -11,9 +11,12 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
+		-- Formatting
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+
+		-- Linting
 		diagnostics.flake8,
 		diagnostics.eslint,
 	},
